3d5e29a5786f6e7b7d46dd565eaedcce01cf2e73
SebastianDrako <sguerrero.sgc@gmail.com>
2023-05-25 16:13:08 -0500

    Se agregaron las librerias como submudulos, de inicio el proyecto de arduino y se preparo el main.ino para trabajarlo

---
+++ arduino_code/main/main.ino
@@ -0,0 +1,26 @@
+#include <Layer1_LoRa.h>
+#include <LoRaLayer2.h>
+
+#include <RF24.h>
+#include <RF24Network.h>
+#include <SPI.h>
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
